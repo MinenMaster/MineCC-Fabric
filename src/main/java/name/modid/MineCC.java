116d1aa04d5b28fc8a102827824b6955e434e0be
Dominik Meister <dominik.s.meister@gmail.com>
2024-04-13 01:12:02 +0200

    Module functionality added

--- src/main/java/name/modid/MineCC.java
+++ src/main/java/name/modid/MineCC.java
@@ -1,7 +1,11 @@
 package name.modid;
 
+import name.modid.module.Mod;
+import name.modid.module.ModuleManager;
 import net.fabricmc.api.ModInitializer;
 
+import net.minecraft.client.MinecraftClient;
+import org.lwjgl.glfw.GLFW;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -10,13 +14,29 @@ public class MineCC implements ModInitializer {
 	// It is considered best practice to use your mod id as the logger's name.
 	// That way, it's clear which mod wrote info, warnings, and errors.
     public static final Logger LOGGER = LoggerFactory.getLogger("minecc");
+	public static final MineCC INSTANCE = new MineCC();
+
+	private MinecraftClient mc = MinecraftClient.getInstance();
 
 	@Override
 	public void onInitialize() {
-		// This code runs as soon as Minecraft is in a mod-load-ready state.
-		// However, some things (like resources) may still be uninitialized.
-		// Proceed with mild caution.
+	}
 
-		LOGGER.info("Hello Fabric world!");
+	public void onKeyPress(int key, int action) {
+		if (action == GLFW.GLFW_PRESS) {
+			for (Mod module : ModuleManager.INSTANCE.getModules()) {
+				if (key == module.getKey()) {
+					module.toggle();
+				}
+			}
+		}
+	}
+
+	public void onTick() {
+		if (mc.player == null) {
+			for (Mod module : ModuleManager.INSTANCE.getEnabledModules()) {
+				module.onTick();
+			}
+		}
 	}
 }
