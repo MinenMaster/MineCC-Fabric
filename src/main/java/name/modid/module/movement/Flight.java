8c572af9d18770b2102ee9d90b4d355ee8200313
Dominik Meister <dominik.s.meister@gmail.com>
2024-04-13 01:12:38 +0200

    Flight module added

---
+++ src/main/java/name/modid/module/movement/Flight.java
@@ -0,0 +1,30 @@
+package name.modid.module.movement;
+
+import name.modid.module.Mod;
+import org.lwjgl.glfw.GLFW;
+
+public class Flight extends Mod {
+    public Flight() {
+        super("Flight", "Allows you to fly", Category.MOVEMENT);
+        this.setKey(GLFW.GLFW_KEY_G);
+    }
+
+    @Override
+    public void onTick() {
+        super.onTick();
+    }
+
+    @Override
+    public void onEnable() {
+//        mc.player.getAbilities().allowFlying = true;
+        mc.player.getAbilities().setFlySpeed(0.1F);
+        mc.player.getAbilities().flying = true;
+    }
+
+    @Override
+    public void onDisable() {
+//        mc.player.getAbilities().allowFlying = false;
+        mc.player.getAbilities().setFlySpeed(0.05F);
+        mc.player.getAbilities().flying = false;
+    }
+}
