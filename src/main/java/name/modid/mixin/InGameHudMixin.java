a2fea8400a2d1942f401fc5025b520e665153b70
Dominik Meister <dominik.s.meister@gmail.com>
2024-04-14 01:21:21 +0200

    imports cleaned

--- src/main/java/name/modid/mixin/InGameHudMixin.java
+++ src/main/java/name/modid/mixin/InGameHudMixin.java
@@ -3,7 +3,6 @@ package name.modid.mixin;
 import name.modid.ui.Hud;
 import net.minecraft.client.gui.DrawContext;
 import net.minecraft.client.gui.hud.InGameHud;
-import net.minecraft.client.util.math.MatrixStack;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
