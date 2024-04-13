6e40ac11477b0765c33b4a9560ba7a4901bce1b8
Dominik Meister <dominik.s.meister@gmail.com>
2024-04-14 01:19:53 +0200

    displayName added

--- src/main/java/name/modid/module/movement/Flight.java
+++ src/main/java/name/modid/module/movement/Flight.java
@@ -5,7 +5,7 @@ import org.lwjgl.glfw.GLFW;
 
 public class Flight extends Mod {
     public Flight() {
-        super("Flight", "Allows you to fly", Category.MOVEMENT);
+        super("Flight", "Flight", "Allows you to fly", Category.MOVEMENT);
         this.setKey(GLFW.GLFW_KEY_G);
     }
 
