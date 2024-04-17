0de3d02c5054e8ca857c261f0c3f35a27b382250
Dominik Meister <dominik.s.meister@gmail.com>
2024-04-17 17:31:10 +0200

    reverted back to previous MinecraftClientMixin

--- src/main/java/name/modid/mixin/MinecraftClientMixin.java
+++ src/main/java/name/modid/mixin/MinecraftClientMixin.java
@@ -5,20 +5,36 @@ import name.modid.module.Mod;
 import name.modid.module.ModuleManager;
 import name.modid.module.world.FastUse;
 import net.minecraft.client.MinecraftClient;
+import net.minecraft.client.network.ClientPlayerInteractionManager;
 import net.minecraft.item.ItemStack;
 import net.minecraft.util.Hand;
+import net.minecraft.util.profiler.Profiler;
+import org.jetbrains.annotations.Nullable;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 import org.spongepowered.asm.mixin.injection.callback.LocalCapture;
 
 @Mixin(MinecraftClient.class)
-public class MinecraftClientMixin {
+public abstract class MinecraftClientMixin {
 
-    @Shadow
-    private int itemUseCooldown;
+    @Unique private boolean doItemUseCalled;
+    @Unique private boolean rightClick;
+
+    @Shadow protected abstract void doItemUse();
+    @Shadow public abstract Profiler getProfiler();
+
+    @Shadow @Nullable public ClientPlayerInteractionManager interactionManager;
+
+    @Shadow private int itemUseCooldown;
+
+    @Inject(method = "<init>", at = @At("TAIL"))
+    private void onInit(CallbackInfo info) {
+        MineCC.INSTANCE.onInitializeClient();
+    }
 
     @Inject(method = "tick", at = @At("HEAD"), cancellable = true)
     private void onTick(CallbackInfo ci) {
