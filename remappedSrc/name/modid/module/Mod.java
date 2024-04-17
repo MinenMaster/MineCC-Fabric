d23a309f144a89f333a6d1b300793b97385df806
Dominik Meister <dominik.s.meister@gmail.com>
2024-04-17 18:25:13 +0200

    remappedSrc files added

---
+++ remappedSrc/name/modid/module/Mod.java
@@ -0,0 +1,107 @@
+package name.modid.module;
+
+import net.minecraft.client.MinecraftClient;
+
+public class Mod {
+    private String name;
+    private String displayName;
+    private String description;
+    public Category category;
+    private int key;
+    private boolean enabled;
+
+    protected MinecraftClient mc = MinecraftClient.getInstance();
+
+    public Mod(String name, String displayName, String description, Category category) {
+        this.name = name;
+        this.displayName = displayName;
+        this.description = description;
+        this.category = category;
+    }
+
+    public void toggle() {
+        this.enabled = !this.enabled;
+
+        if (this.enabled) {
+            onEnable();
+        } else {
+            onDisable();
+        }
+    }
+
+    public void onEnable() {
+
+    }
+
+    public void onDisable() {
+
+    }
+
+    public void onTick() {
+
+    }
+
+    public enum Category {
+        COMBAT("Combat"),
+        MOVEMENT("Movement"),
+        RENDER("Render"),
+        WORLD("World"),
+        MISC("Misc");
+
+        public String name;
+
+        private Category(String name) {
+            this.name = name;
+        }
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDisplayName() {
+        return displayName;
+    }
+
+    public void setDisplayName(String displayName) {
+        this.displayName = displayName;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Category getCategory() {
+        return category;
+    }
+
+    public int getKey() {
+        return key;
+    }
+
+    public void setKey(int key) {
+        this.key = key;
+    }
+
+    public boolean isEnabled() {
+        return enabled;
+    }
+
+    public void setEnabled(boolean enabled) {
+        this.enabled = enabled;
+
+        if (this.enabled) {
+            onEnable();
+        } else {
+            onDisable();
+        }
+    }
+}
