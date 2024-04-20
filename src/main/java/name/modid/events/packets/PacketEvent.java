505e4a696f380d3472699015180d68980153f28b
Dominik Meister <dominik.s.meister@gmail.com>
2024-04-20 23:01:18 +0200

    Receive and Sent method added

--- src/main/java/name/modid/events/packets/PacketEvent.java
+++ src/main/java/name/modid/events/packets/PacketEvent.java
@@ -1,18 +1,43 @@
 package name.modid.events.packets;
 
 import name.modid.events.Cancellable;
+import net.minecraft.network.listener.PacketListener;
 import net.minecraft.network.packet.Packet;
 
 public class PacketEvent {
 
-    public static class Send extends Cancellable {
+    public static class Receive extends Cancellable {
+        private static final Receive INSTANCE = new Receive();
 
+        public Packet<?> packet;
+        public PacketListener packetListener;
+
+        public static Receive get(Packet<?> packet, PacketListener listener) {
+            INSTANCE.setCancelled(false);
+            INSTANCE.packet = packet;
+            INSTANCE.packetListener = listener;
+            return INSTANCE;
+        }
+    }
+
+    public static class Send extends Cancellable {
         private static final Send INSTANCE = new Send();
 
         public Packet<?> packet;
 
         public static Send get(Packet<?> packet) {
             INSTANCE.setCancelled(false);
+            INSTANCE.packet = packet;
+            return INSTANCE;
+        }
+    }
+
+    public static class Sent {
+        private static final Sent INSTANCE = new Sent();
+
+        public Packet<?> packet;
+
+        public static Sent get(Packet<?> packet) {
             INSTANCE.packet = packet;
             return INSTANCE;
         }
